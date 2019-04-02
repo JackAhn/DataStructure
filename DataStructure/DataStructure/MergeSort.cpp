467cc42bbc918f41f2c7513569599e6a1337a431
DoHyeon Ahn <an71195646@gmail.com>
2019-04-02 11:10:37 +0900

    complete mergesort & start heapsort

--- DataStructure/DataStructure/MergeSort.cpp
+++ DataStructure/DataStructure/MergeSort.cpp
@@ -3,7 +3,77 @@
 using namespace std;
 
 int main(void) {
-	int arr[10] = { 1,10,9,5,6,3,2,4,8,7 };
-	int mul = 2; //2의 배수씩 증가
+	int much = 0;
+	cin >> much;
+	int * arr = new int[much];
+	int * answer = new int[much];
 
+	for (int i = 0; i < much; i++) {
+		cin >> arr[i];
+	}
+
+	int len = 1; //분할 배열 개수
+	int mul = 2; //2의 배수씩 증가
+	int i = 0, j = 0, ct = 0, ind = 0;
+	int iflag = 0, jflag = 0;
+	while (1) {
+		if (mul > much)
+			break;
+		ind = 0;
+		ct = 0;
+		i = 0;
+		j = len;
+		iflag = i + len;
+		jflag = j + len;
+		while (j < much) {
+			ct++;
+			if (arr[i] > arr[j]) {
+				answer[ind++] = arr[j];
+				j++;
+			}
+
+			else if (arr[i] < arr[j]) {
+				answer[ind++] = arr[i];
+				i++;
+			}
+			if (ct >= len) {
+				if (i == iflag) {
+					int c = 0;
+					while (c < jflag - j) {
+						answer[ind++] = arr[j++];
+						c++;
+					}
+					i += len;
+					j = i + len;
+					iflag = i + len;
+					jflag = j + len;
+					ct = 0;
+				}
+
+				else if (j == jflag) {
+					int c = 0;
+					while (c < iflag - i) {
+						answer[ind++] = arr[i++];
+						c++;
+					}
+					i = j;
+					j += len;
+					iflag = i + len;
+					jflag = j + len;
+					ct = 0;
+				}
+			}
+
+		}
+		for (int i = 0; i < much; i++) {
+			arr[i] = answer[i];
+		}
+		len = mul;
+		mul *= 2;
+	}
+	for (int i = 0; i < much; i++) {
+		cout << arr[i] << ' ';
+	}
+	delete[] arr;
+	delete[] answer;
 }
