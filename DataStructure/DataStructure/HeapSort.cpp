1d528c704df429b827f68741f4216589e835c5b3
DoHyeon Ahn <an71195646@gmail.com>
2019-04-10 10:39:31 +0900

    Finish HeapSort

---
+++ DataStructure/DataStructure/HeapSort.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+
+int Heapify(int *arr, int parentind, int maxind) { //Èü Á¤·Ä ¼öÇà
+	int parentdata = arr[parentind];
+	if ((parentind * 2) <= maxind) {
+		int data = Heapify(arr, parentind * 2, maxind);
+		if (parentdata < data) {
+			arr[parentind] = data;
+			arr[parentind * 2] = parentdata;
+			Heapify(arr, parentind, maxind);
+			return data;
+		}
+	}
+	else {
+		return parentdata;
+	}
+
+	if ((parentind * 2) + 1 <= maxind) {
+		int data = Heapify(arr, (parentind * 2) + 1, maxind);
+		if (parentdata < data) {
+			arr[parentind] = data;
+			arr[(parentind * 2) + 1] = parentdata;
+			Heapify(arr, parentind, maxind);
+			return data;
+		}
+	}
+	else {
+		return parentdata;
+	}
+	return parentdata;
+}
+
+//ÃÖ´ë Èü ±âÁØ
+int main(void) {
+	int much = 0;
+	cin >> much;
+	int * arr = new int[much + 1];
+	for (int i = 1; i < much + 1; i++) {
+		cin >> arr[i];
+	}
+
+	Heapify(arr, 1, much + 1);
+	for (int i = 1; i < much + 1; i++) {
+		cout << arr[i] << ' ';
+	}
+	delete[] arr;
+}
