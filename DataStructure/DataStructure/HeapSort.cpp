1c9f15b4b058ada4dd657599f902c3aaa687449b
DoHyeon Ahn <an71195646@gmail.com>
2019-05-07 22:36:22 +0900

    Study BFS

--- DataStructure/DataStructure/HeapSort.cpp
+++ DataStructure/DataStructure/HeapSort.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 
-
-int Heapify(int *arr, int parentind, int maxind) { //Èü Á¤·Ä ¼öÇà
+/*int Heapify(int *arr, int parentind, int maxind) {
 	int parentdata = arr[parentind];
 	if ((parentind * 2) <= maxind) {
 		int data = Heapify(arr, parentind * 2, maxind);
@@ -31,9 +30,32 @@ int Heapify(int *arr, int parentind, int maxind) { //Èü Á¤·Ä ¼öÇà
 		return parentdata;
 	}
 	return parentdata;
+}*/
+
+int Heapify(int *arr, int parentind, int maxind) {
+	int parentdata = arr[parentind];
+	if ((parentind * 2) <= maxind) {
+		int data = Heapify(arr, parentind * 2, maxind);
+		int data2 = Heapify(arr, (parentind * 2) + 1, maxind);
+		if (parentdata < data) {
+			arr[parentind] = data;
+			arr[parentind * 2] = parentdata;
+			Heapify(arr, parentind * 2, maxind);
+			return data;
+		}
+		else if (parentdata < data2) {
+			arr[parentind] = data;
+			arr[(parentind * 2) + 1] = parentdata;
+			Heapify(arr, (parentind * 2) + 1, maxind);
+			return data;
+		}
+	}
+	else {
+		return parentdata;
+	}
+	return parentdata;
 }
 
-//ÃÖ´ë Èü ±âÁØ
 int main(void) {
 	int much = 0;
 	cin >> much;
@@ -43,6 +65,7 @@ int main(void) {
 	}
 
 	Heapify(arr, 1, much + 1);
+
 	for (int i = 1; i < much + 1; i++) {
 		cout << arr[i] << ' ';
 	}
